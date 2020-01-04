2fe81f76519a9421d49e8f1eba82f02e9b5c87fe
Zhicheng Zhang <zhangzhicheng1@126.com>
2020-01-04 12:54:48 +0800

    revert...

--- RISCV_CPU/run_test_fpga.sh
+++ RISCV_CPU/run_test_fpga.sh
@@ -7,6 +7,6 @@ if [ -f ./testcase/$@.in ]; then cp ./testcase/$@.in ./test/test.in; fi
 if [ -f ./testcase/$@.ans ]; then cp ./testcase/$@.ans ./test/test.ans; fi
 
 ./ctrl/build.sh
-# ./ctrl/run.sh ./test/test.bin ./test/test.in /dev/ttyS4 -I
-./ctrl/run.sh ./test/test.bin ./test/test.in /dev/ttyS4 -T > ./test/test.out
+# ./ctrl/run.sh ./test/test.bin ./test/test.in /dev/ttyS6 -I
+./ctrl/run.sh ./test/test.bin ./test/test.in /dev/ttyS6 -T > ./test/test.out
 if [ -f ./test/test.ans ]; then diff ./test/test.ans ./test/test.out; fi
